3bdc08297785f88f797b42a4538ca103fa0fd91b
flo <roucairol.florian@gmail.com>
2018-08-02 15:17:50 +0200

    modif

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    <title>Je suis modifié</title>
+    <title>Je test les modifs</title>
   </head>
   <body>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
